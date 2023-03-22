30d57cf21854a7b59f33ba6e2254966caede977c
nekithrill <98217583+nekithrill@users.noreply.github.com>
2023-03-22 14:59:33 +0200

    init commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+import '../scss/index.scss'
+// import '../css/style.css'
+
+console.log('Hello world!')
---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+// webpack.config.js
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const webpackConfig = {
+	mode: 'production',
+	entry: {
+		main: path.resolve(__dirname, './src/js/index.js')
+	},
+
+	output: {
+		filename: 'index.bundle.js',
+		path: path.resolve(__dirname, './dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+			}
+		]
+	},
+
+	plugins: [
+		new CleanWebpackPlugin(),
+
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: 'assets/images/*.jpg',
+					to: '',
+					context: 'src/'
+				}
+			]
+		}),
+
+		new MiniCssExtractPlugin({
+			filename: 'css/style.css'
+		}),
+
+		new HtmlWebpackPlugin({
+			title: 'webpack index',
+			template: './src/pages/index.html',
+			filename: 'pages/index.html',
+			inject: true,
+			chunks: ['pages'],
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true
+			}
+		}),
+
+		new HtmlWebpackPlugin({
+			title: 'webpack news',
+			template: './src/pages/news.html',
+			filename: 'pages/news.html',
+			inject: true,
+			chunks: ['pages'],
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true
+			}
+		}),
+
+		new HtmlWebpackPlugin({
+			title: 'webpack photo',
+			template: './src/pages/photo.html',
+			filename: 'pages/photo.html',
+			inject: true,
+			chunks: ['pages'],
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true
+			}
+		}),
+
+		new HtmlWebpackPlugin({
+			title: 'webpack rozklad',
+			template: './src/pages/rozklad.html',
+			filename: 'pages/rozklad.html',
+			inject: true,
+			chunks: ['pages'],
+			minify: {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeRedundantAttributes: true,
+				removeScriptTypeAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				useShortDoctype: true
+			}
+		})
+	]
+}
+
+module.exports = webpackConfig
